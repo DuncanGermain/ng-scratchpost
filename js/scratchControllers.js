85134fa61daf92c065d5158b779e3f0784f8a075
DuncanGermain <TK17Studios@gmail.com>
2015-05-19 16:22:00 -0700

    Adding form functionality to start (e.g. disabled buttons unless content entered).

--- js/scratchControllers.js
+++ js/scratchControllers.js
@@ -15,16 +15,16 @@ scratchControllers.controller('StartControl', [
 		$scope.startButtons = true;
 		$scope.startOptions = false;
 		$scope.joinOptions = false;
-		$scope.msg = '';
+		$scope.sysMsg = '';
 		$scope.newSess = function() {
 			$scope.startButtons = false;
 			$scope.startOptions = true;
-			$scope.msg = "Please create a session code.";
+			$scope.sysMsg = "Please create a session code.";
 		};
 		$scope.joinSess = function() {
 			$scope.startButtons = false;
 			$scope.joinOptions = true;
-			$scope.msg = "Please enter the session code and a username."
+			$scope.sysMsg = "Please enter the session code and a username."
 		};
 
 		//Functionality for starting a new session
@@ -38,6 +38,7 @@ scratchControllers.controller('StartControl', [
 				checkboxes: {noAnon: false, noRedo: false, noShow: false}
 			};
 			sessRef.update(initObj);
+			$location.path('/leader');
 		};
 
 		//Functionality for joining an existing session
@@ -46,25 +47,27 @@ scratchControllers.controller('StartControl', [
 			userID = $scope.userID;
 			var unique;
 			function checkSessID(entryID, exists, unique) {
-				if (exists && unique) {
-					//Creates default object for a new user with the chosen name
+				if (!exists) {
+					$scope.sysMsg = "Sorry, we can't find a session with that code. Please try again, or contact your session leader for help."
+				} else if (exists && !unique) {
+					$scope.sysMsg = "Sorry, that username has already been taken for this session. Please choose another."
+				} else {
 					var userObj = {};
 					userObj[userID] = {data: 'New user!'};
 					sessRef.child(entryID).child('users').update(userObj);
 					$location.path('/partic');
-				} else if (exists && !unique) {
-					$scope.msg = "Sorry, that username has already been taken for this session. Please choose another."
-				} else {
-					$scope.msg = "Sorry, we can't find a session with that code. Please try again, or contact your session leader for help."
 				}
+				$scope.$apply();
 			}
 			//Confirms that there is no data stored at the path corresponding to the entered username
 			sessRef.child(entryID).child('users').child(userID).once('value', function(snapshot) {
 				unique = (snapshot.val() === null);
+				console.log(unique);
 			});
 			//Confirms that there IS data stored at the path corresponding to the entered session code
 			sessRef.child(entryID).once('value', function(snapshot) {
 				var exists = (snapshot.val() !== null);
+				console.log(exists);
 				checkSessID(entryID, exists, unique);
 			});
 		}
